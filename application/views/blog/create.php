d9e893152389fda8a83a47f4d1c06e3707376835
연서은 <ysedeveloper@gmail.com>
2024-12-30 17:45:10 +0900

    04.01. 블로그 글쓰기 화면

---
+++ application/views/blog/create.php
@@ -0,0 +1,10 @@
+<h2>새 글 쓰기</h2>
+<form method="POST">
+ <label for="title">제목</label>
+ <input type="text" id="title" name="title" /><br />
+
+
+ <label for="body">본문</label>
+ <textarea id="body" name="body"></textarea><br />
+ <input type="submit" name="submit" value="새 글 쓰기" />
+</form>
