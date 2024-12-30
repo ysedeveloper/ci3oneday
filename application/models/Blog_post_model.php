f3afafacac35fcdc51a55edbda32299f744d0d9d
연서은 <ysedeveloper@gmail.com>
2024-12-31 03:46:58 +0900

    04.02.07. 모델 메서드 작성

--- application/models/Blog_post_model.php
+++ application/models/Blog_post_model.php
@@ -4,5 +4,17 @@ class Blog_model extends CI_Model {
     public function __construct()
     {
         $this->load->database();
-    }    
+    }
+    
+    public function insert($title, $body){
+        $data = [
+            "title" => $title,
+            "body" => $body
+        ];
+        
+        $this->db->insert("blog_post", $data);
+        
+        return $this->db->insert_id();
+    }
+    
 }
